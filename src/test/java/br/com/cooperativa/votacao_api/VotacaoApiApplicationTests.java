efd43365be65c2bc3456e386ed69abfeb1fbce90
Bruno <bruno8117@gmail.com>
2025-08-15 17:44:43 -0300

    feat: Initial project structure from Spring Initializr

---
+++ src/test/java/br/com/cooperativa/votacao_api/VotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cooperativa.votacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
